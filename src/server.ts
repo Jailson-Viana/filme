735efb09c17cbb3b39dd2b198dbb264c183513fb
jailson-viana <jailson2015viana@gmail.com>
2024-09-26 20:37:23 -0300

    Adicionado a configuração de erro na criação de filmes

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from '@prisma/client';
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
+app.use(express.json());
 
 app.get('/movies', async (_, res) => {
     const movies = await prisma.movie.findMany({
@@ -11,11 +12,31 @@ app.get('/movies', async (_, res) => {
             title: 'asc',
         },
         include: {
-            genres: true, 
-            languages: true
+            genres: true,
+            languages: true,
         },
     });
     res.json(movies);
+});
+
+app.post('/movies', async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+    try{
+    await prisma.movie.create({
+        data: {
+            title,
+            genre_id,
+            language_id,
+            oscar_count,
+            // cuidado aqui, o mes começa em 0 e vai até 11
+            release_date: new Date(release_date),
+        },
+    });
+}catch(erro){
+    return res.status(500).send({message:"Falha ao cadastrar um filme"})
+
+}
+    res.status(201).send();
 });
 
 app.listen(port, () => {
