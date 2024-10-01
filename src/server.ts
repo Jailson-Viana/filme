5e9b52bd08f4ed47a8f08e4ad96b8dfb79ff99d5
jailson-viana <jailson2015viana@gmail.com>
2024-10-01 12:00:04 -0300

    Adicionado a configuração de atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -19,13 +19,42 @@ app.get('/movies', async (_, res) => {
    res.json(movies);
 });
 
+app.put('/movies/:id', async (req, res) => {
+   //pegar o id do registro que será atualizado
+   const id = Number(req.params.id);
+   try {
+      const movie = await prisma.movie.findUnique({
+         where: { id },
+      });
+
+      if (!movie) {
+         return res.status(404).send({ message: 'Filme não encontrado!' });
+      }
+      const data = { ...req.body };
+      data.release_date = data.release_date ? new Date(data.release_date) : undefined;
+
+      //pegar os dados do filme que será atualizado e atualizar ele no prisma
+      await prisma.movie.update({
+         where: {
+            id,
+         },
+         data: data,
+      });
+   } catch (error) {
+      return res.status(500).send({ message: 'Falha ao atualizar o resgitro do filme!' });
+   }
+
+   //retornar o status correto informando que o filme foi atualizado
+   res.status(200).send();
+});
+
 app.post('/movies', async (req, res) => {
    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
    try {
-      /*case insensitive - se a busca dor feita por john wick ou John wick ou JOHN WICK, 
+      /*case insensitive - se a busca for feita por john wick ou John wick ou JOHN WICK, 
       o registro vai ser retornado na consulta.*/
 
-      /*case sensitive - se buscar por jhon wick e no banco estiver com Jhon wick, não vai ser retornado na consulta*/
+      /*case sensitive - se buscar for jhon wick e no banco estiver com Jhon wick, não vai ser retornado na consulta*/
 
       const movieWithSameTitle = await prisma.movie.findFirst({
          where: { title: { equals: title, mode: 'insensitive' } },
